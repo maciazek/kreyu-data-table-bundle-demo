8aefd1bd726908de86c3901b0dcb57f7b43a1af2
Maciej Maciąg <maciejmaciag@intmail.pl>
2025-06-17 20:32:16 +0200

    Update dependencies

--- importmap.php
+++ importmap.php
@@ -28,25 +28,25 @@ return [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
     'bootstrap' => [
-        'version' => '5.3.5',
+        'version' => '5.3.7',
     ],
     '@popperjs/core' => [
         'version' => '2.11.8',
     ],
     'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.5',
+        'version' => '5.3.7',
         'type' => 'css',
     ],
     '@fontsource/noto-sans/latin.css' => [
-        'version' => '5.2.6',
+        'version' => '5.2.7',
         'type' => 'css',
     ],
     '@fontsource/noto-sans/latin-ext.css' => [
-        'version' => '5.2.6',
+        'version' => '5.2.7',
         'type' => 'css',
     ],
     'bootstrap-icons/font/bootstrap-icons.min.css' => [
-        'version' => '1.11.3',
+        'version' => '1.13.1',
         'type' => 'css',
     ],
     '@hotwired/stimulus' => [
@@ -56,19 +56,19 @@ return [
         'version' => '1.15.6',
     ],
     '@fontsource/roboto-mono/latin.css' => [
-        'version' => '5.2.5',
+        'version' => '5.2.6',
         'type' => 'css',
     ],
     '@fontsource/roboto-mono/latin-ext.css' => [
-        'version' => '5.2.5',
+        'version' => '5.2.6',
         'type' => 'css',
     ],
     '@tabler/icons-webfont/dist/tabler-icons.min.css' => [
-        'version' => '3.31.0',
+        'version' => '3.34.0',
         'type' => 'css',
     ],
     '@tabler/core' => [
-        'version' => '1.2.0',
+        'version' => '1.3.2',
     ],
     'autosize' => [
         'version' => '6.0.1',
@@ -77,16 +77,16 @@ return [
         'version' => '7.6.1',
     ],
     '@tabler/core/dist/css/tabler.min.css' => [
-        'version' => '1.2.0',
+        'version' => '1.3.2',
         'type' => 'css',
     ],
     '@hotwired/turbo' => [
         'version' => '8.0.13',
     ],
     'chart.js' => [
-        'version' => '4.4.9',
+        'version' => '4.5.0',
     ],
     '@kurkle/color' => [
-        'version' => '0.3.4',
+        'version' => '0.4.0',
     ],
 ];
